f080fe0aa704bd81b1dfdb6b672432c29f772e17
Samarth Zalte <52416785+samz905@users.noreply.github.com>
2024-07-23 01:59:41 +0530

    Final issues in Neuron and Layer classes fixed

--- components/nn.py
+++ components/nn.py
@@ -27,6 +27,7 @@ class Layer:
         outputs = [neuron(x) for neuron in self.neurons]
 
         # The below step converts the list of neuron outputs into a single vectorised unit while incorporating backprop through the custom concat function defined for the Unit class
+        # Each unit in the input of this concat operation is the output of the respective neuron in the layer stored as a list. Once each neuron gets its gradient, it backprops to the weights as well through w * x + b
         return outputs[0] if len(outputs) == 1 else Unit.concat(outputs) # Converts a list of units into a single vectorised unit
 
 
